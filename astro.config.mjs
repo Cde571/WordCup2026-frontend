432f0528017b80fe92ca8853768d65ae9438f2f3
Cde571 <Cristian.echeverry1@udea.edu.co>
2025-12-18 22:58:08 -0500

    api-wordcu26p

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
